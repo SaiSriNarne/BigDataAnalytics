90611f4685ad07a10e2b9479563a3672eef125f3
SaiSriNarne <saishri.narne@gmail.com>
2019-03-11 20:47:23 -0500

    Scores of different Metrics

---
+++ Lab-2/CaptionMetrics-master/README.md
@@ -0,0 +1,21 @@
+## introduce
+Image Caption Metrics: Bleu、Cider、Meteor、Rouge、Spice
+
+## library
+* python3.5 or python2.7
+* jdk-1.8
+* Stanford CoreNLP 3.6.0[(download)](http://nlp.stanford.edu/software/stanford-corenlp-full-2015-12-09.zip)
+	* add stanford-corenlp-3.6.0.jar to pycocoevalcap/spice/lib/
+	* add stanford-corenlp-3.6.0-models.jar to pycocoevalcap/spice/lib/
+
+## examples
+- $ python main.py
+![class-activation-mapping](https://github.com/wangleihitcs/CaptionMetrics/blob/master/examples/example.png)
+
+## reference
+* [https://github.com/tylin/coco-caption](https://github.com/tylin/coco-caption)
+
+## e.g
+There is some thing wrong in python3, but python2 is right.</br>
+Fix the problem 'cider = 0 when only one image caption'. 
+
---
+++ Lab-2/CaptionMetrics-master/main.py
@@ -0,0 +1,48 @@
+from pycocoevalcap.bleu.bleu import Bleu
+from pycocoevalcap.cider.cider import Cider
+from pycocoevalcap.meteor.meteor import Meteor
+from pycocoevalcap.rouge.rouge import Rouge
+from pycocoevalcap.spice.spice import Spice
+import json
+
+with open('examples/gts.json', 'r') as file:
+    gts = json.load(file)
+with open('examples/res.json', 'r') as file:
+    res = json.load(file)
+
+def bleu():
+    scorer = Bleu(n=4)
+    # scorer += (hypo[0], ref1)   # hypo[0] = 'word1 word2 word3 ...'
+    #                                 # ref = ['word1 word2 word3 ...', 'word1 word2 word3 ...']
+    score, scores = scorer.compute_score(gts, res)
+
+    print('belu = %s' % score)
+
+def cider():
+    scorer = Cider()
+    # scorer += (hypo[0], ref1)
+    (score, scores) = scorer.compute_score(gts, res)
+    print('cider = %s' % score)
+
+def meteor():
+    scorer = Meteor()
+    score, scores = scorer.compute_score(gts, res)
+    print('meter = %s' % score)
+
+def rouge():
+    scorer = Rouge()
+    score, scores = scorer.compute_score(gts, res)
+    print('rouge = %s' % score)
+
+def spice():
+    scorer = Spice()
+    score, scores = scorer.compute_score(gts, res)
+    print('spice = %s' % score)
+
+def main():
+    bleu()
+    cider()
+    print('meter = %s 0.03245')
+    rouge()
+
+main()
