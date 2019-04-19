5c544934c496e1bbb5754264561fa9350efc296d
SaiSriNarne <saishri.narne@gmail.com>
2019-04-19 13:32:05 -0500

    supervised image classification

---
+++ ICP10/src/main/scala/IPSettings.scala
@@ -0,0 +1,14 @@
+/**
+  * Created by pradyumnad on 19/07/15.
+  */
+object IPSettings {
+
+  val PATH = "data/"
+  val INPUT_DIR = PATH + "train1"
+  val TEST_INPUT_DIR = PATH + "test1"
+  val FEATURES_PATH = PATH + "model/features"
+
+  val KMEANS_PATH = PATH + "model/clusters"
+  val HISTOGRAM_PATH = PATH + "model/histograms"
+  val RANDOM_FOREST_PATH = PATH + "model/nbmodel"
+}
